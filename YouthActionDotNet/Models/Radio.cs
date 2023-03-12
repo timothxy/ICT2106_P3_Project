0032d18bdb3bd363f888c0d12a51c897fe76a96f
LynxYufloria <94039945+LynxYufloria@users.noreply.github.com>
2023-03-12 08:22:49 +0800

    Create Radio.cs

---
+++ YouthActionDotNet/Models/Radio.cs
@@ -0,0 +1,15 @@
+using System;
+using Newtonsoft.Json;
+
+namespace YouthActionDotNet.Models{
+
+    public class Radio : Question {
+        
+        public string questionContent  { get; set; }
+        
+        public override void setAnswer()
+        {
+          // To be implemented
+        }
+    }
+}
